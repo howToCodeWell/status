8ed5951cadfdd5179f804442a74d8fe46850a1c6
peterfisher <c3T-y7J-vo3-qpg>
2019-10-24 10:39:12 +0100

    Adding controller code

---
+++ site/app/Http/Controllers/SiteController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Site;
+use Illuminate\Routing\Controller as BaseController;
+
+class SiteController extends BaseController
+{
+    public function index()
+    {
+        return view('site.index', ['sites' => Site::all(), 'results' => []]);
+    }
+}
---
+++ site/resources/views/site/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sites</title>
+</head>
+<body>
+<table>
+    <thead>
+    <tr>
+        <td>Site Name</td>
+        <td>Result</td>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($sites as $site)
+    <tr>
+        <td>
+            <a href="{{ $site->url }}">{{$site->name}}</a>
+        </td>
+        <td>--</td>
+    </tr>
+    @endforeach
+    </tbody>
+</table>
+</body>
+</html>
